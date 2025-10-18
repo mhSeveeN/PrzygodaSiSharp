6d4c9060be5ce00a1392a8988b941f125324d200
SeveeN <maciej.hudek@gmail.com>
2025-10-18 19:43:04 +0200

    First app C$#
    My first steps in C#

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        //Console.WriteLine("Hello Maciej!");
+        Int32 number1 = 5;
+        Int32 number2 = 10;
+        Console.WriteLine($"Sumą 2+2 jest {2 + 2}");
+        Console.WriteLine($" Sumą {number1} + {number2} jest {number1 + number2}");
+
+        number1 = Int32.Parse(args[0]);
+        number2 = Int32.Parse(args[1]);
+
+        Console.WriteLine($"Sumą {number1} + {number2} jest {number1 + number2}");
+    }
+}

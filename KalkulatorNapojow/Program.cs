beafb3c5a3192d1b6b566e07b67b80f60bb929f6
SeveeN <maciej.hudek@gmail.com>
2025-11-07 17:58:27 +0100

    V2.0 - Dodano małą dokumentację kodu

--- KalkulatorNapojow/Program.cs
+++ KalkulatorNapojow/Program.cs
@@ -1,21 +1,27 @@
-﻿internal class Program
+﻿/*
+   < Program służący jako kalkulator ilości i ceny napojów, działający interaktywnie z użytkownikiem. >
+   < Autor: mhSeveeN >
+   < Date: 07.11.2025r. >
+*/
+internal class Program
 {
     private static void Main(string[] args)
     {
+        //Menu
         Console.WriteLine("====== MENU ======");
         Console.WriteLine("1 - Kawa - 7zł/kubek");
         Console.WriteLine("2 - Herbata - 5zł/kubek");
         Console.WriteLine("3 - Woda - 3zł/500ml");
         Console.WriteLine("4 - Exit");
         Console.WriteLine("Witaj w kalkulatorze napojów!!! ");
-        Console.WriteLine("Wybierz opcję (1 - 4): ");
+        Console.WriteLine("Wybierz opcję (1 - 4): ");//Interakcja z użytkownikiem
 
         //Wczytanie danych od użytkownika
         string input = Console.ReadLine();
 
         if (int.TryParse(input, out int wybor))
         {
-            switch (wybor)
+            switch (wybor) //switch case
             {
                 case 1:
                     Console.WriteLine("Wybrałeś kawę!!");
@@ -45,7 +51,8 @@
                     Console.WriteLine("Do Widzenia!!");
                     break;
             }
-        } else
+        }
+        else
         {
             Console.WriteLine("Ta opcja nie istnieje w MENU! Uruchom program ponownie!!");
         }

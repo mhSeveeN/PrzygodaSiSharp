50d06733c810a6548863404a3dd1ee637843921d
SeveeN <maciej.hudek@gmail.com>
2025-11-08 13:13:59 +0100

    Aplikacja Lotnicza z interakcją wysokościową

---
+++ AplikacjaLotnicza/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("==============================================================================================");
+        Console.WriteLine("|+| Welcome Everybody on our flight. I hope you'll enjoy your work. |+|");
+        Console.WriteLine("|+| I want to ask you What is the height that you're currently flying?:  |+|");
+        int PlaneHeight = int.Parse(Console.ReadLine());
+        if (PlaneHeight > 1000)
+        {
+            Console.WriteLine("|+| You're flying higher than you shouldn't - Please take down a little!! |+|");
+        }
+        else if (PlaneHeight <= 1000 && PlaneHeight > 800)
+        {
+            Console.WriteLine("|+| You're height is optimal, keep going :) |+|");
+        }
+        else
+        {
+            Console.WriteLine("|+| You're too close - PULL UP     PULL UP    PULL UP!!!! |+| ");
+        }
+
+        Console.WriteLine("=================================================================================================");
+    }
+}

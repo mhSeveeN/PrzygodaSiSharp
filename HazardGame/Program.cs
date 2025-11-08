d8369d49a4ae07f832fb56ced4d2771bf7caa3e3
SeveeN <maciej.hudek@gmail.com>
2025-11-08 13:36:51 +0100

    Ruletka na losowanie liczby

---
+++ HazardGame/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random lose = new Random();
+        int liczba = lose.Next(1, 11);
+        Console.WriteLine("Witaj użytkowniku!! Wylosuj liczbę z przedziału <1, 10>");
+        int traf = int.Parse(Console.ReadLine());
+        if (traf == liczba)
+        {
+            Console.WriteLine("Twój traf jest szczęśliwy!!!! To dokładnie ta liczba!!!");
+        } else if (traf < liczba)
+        {
+            Console.WriteLine("Dzisiaj nie masz szczęścia!!  Twój traf jest za mały!!");
+        } else
+        {
+            Console.WriteLine("Nie masz szczęscia!! Twój traf jest za duży!!");
+        }
+    }
+}

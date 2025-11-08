b86403fa5db6d0ecbd8cad4ad736e676d0a212f7
SeveeN <maciej.hudek@gmail.com>
2025-11-08 12:31:03 +0100

    Program: Kim jesteś i ile masz lat

---
+++ PierwszyKod/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("podaj swoje imię: ");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("Witaj " + firstName + "!!");
+        Console.WriteLine("Podaj swój rok urodzenia: ");
+        int userInput1 = int.Parse(Console.ReadLine());
+        int age = 2025 - userInput1;
+        Console.WriteLine("Witaj " + firstName + " , już wiem, że masz " + age + " lat.");
+        if (age <= 18)
+        {
+            Console.WriteLine("Dostęp zablokowany - jesteś niepełnoletni:(");
+        } else
+        {
+            Console.WriteLine("Dostęp odblokowany:)");
+        }
+    }
+}

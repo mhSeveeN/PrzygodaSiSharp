fd7237a82946f0e4016c4a2142dedd2aabb247ba
SeveeN <maciej.hudek@gmail.com>
2025-11-22 19:05:36 +0100

    Kalkulator prosty - dodawanie 2 liczb

---
+++ Kalkulator/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calc = new Calculator();
+        int result = calc.Add(2, 3);
+        Console.WriteLine("Wynik dodawania " + result);
+    }
+}

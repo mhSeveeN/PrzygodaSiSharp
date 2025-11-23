ceee136836aec5d68bc58702e363d6bd11d63293
SeveeN <maciej.hudek@gmail.com>
2025-11-23 12:07:47 +0100

    Program liczący liczbe klientów sklepu

---
+++ LiczbaKlientow/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Witaj w programie mierzącym liczbę klientów w sklepie!!");
+        int[] klienci = {23, 46, 52, 7, 2, 10, 30, 15, 16, 17, 1, 0};
+        int sumaKlientow = 0;
+        int max = klienci[0];
+        int godzina = klienci[0];
+
+        for (int i=0; i < klienci.Length; i++)
+        {
+            if (max < klienci[i] )
+            {
+                max = klienci[i];
+                godzina = i;
+            }   
+            sumaKlientow = sumaKlientow + klienci[i];         
+        }
+        if (max>20 )
+            {
+                Console.WriteLine("Potrzeba dodatkowej obsługi");
+            }
+        Console.WriteLine("Sklep odwiedzilo " + sumaKlientow + " klientów");
+        Console.WriteLine($"Najwięcej klientów było {max} o godzinie {godzina}");
+    }
+}

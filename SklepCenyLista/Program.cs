92119d0cdf6dcb71911072968edfceeabff53a3c
mhSeveeN <maciej.hudek@gmail.com>
2025-11-29 17:27:01 +0100

    [LM] Aplikacja sklepowa z cenami

---
+++ SklepCenyLista/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] produkty = { "Chleb", "Mleko", "Ser", "Kawa" };
+        int[] ceny = { 5, 4, 12, 25 };
+
+        Console.WriteLine("Lista Produktów: ");
+        for (int i = 0; i < produkty.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}. {produkty[i]} - {ceny[i]} PLN");
+        }
+
+        powrot:
+        Console.WriteLine("\nPodaj indeks produktu, aby zobaczyć jego cenę: ");
+        int indeks = int.Parse(Console.ReadLine());
+
+        if (indeks >= 0 && indeks < produkty.Length)
+        {
+            Console.WriteLine($"Wybrałeś: {produkty[indeks]}, cena: {ceny[indeks]} zł");
+        } else
+        {
+            Console.WriteLine("Niepoprawny Indeks!!");
+            goto powrot;
+        }
+
+        Console.WriteLine("\nPodaj ilość wybranego produktu: ");
+        int liczbaProdukt = int.Parse(Console.ReadLine());
+        int cenaProdukt = ceny[indeks] * liczbaProdukt;
+        Console.WriteLine($"Łączna cena za produkt {produkty[indeks]} wynosi: {cenaProdukt} zł");        
+    }
+}
